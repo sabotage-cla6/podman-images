17866b59574666d08ea0bcf3c3f136291beaef07
sabotagecla6 <nine.gates111999@gmail.com>
2025-06-24 00:40:14 +0900

    SabotageCla6/chrome
    Chrome実行のベースイメージ

--- create-images.sh
+++ create-images.sh
@@ -5,3 +5,6 @@ podman build ./100_base/ -t sabotagecla6/ubuntu
 podman build ./200_usr/  -t sabotagecla6/ubuntu-usr
 podman build ./300_gui/  -t sabotagecla6/ubuntu-gui
 podman build ./300_gui/  -t sabotagecla6/ubuntu-gui-jp -f ./300_gui/Containerfile_JP
+
+# application
+podman build ./500_application/chrome  -t sabotagecla6/chrome
