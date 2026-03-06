5f06e3d0cff8cf83c61e8c406deb38d9a22d31a1
SabotageCla6 <nine.gates111999@gmail.com>
2026-03-06 23:59:04 +0900

    apt cleanなどを実行するように変更

--- 300_gui/entrypoint.sh
+++ 300_gui/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-export XAUTHORITY=$HOME/.Xauthority
+# export XAUTHORITY=$HOME/.Xauthority
 if [ -f /tmp/.Xauthority ];then
     sudo cp /tmp/.Xauthority $XAUTHORITY
+    sudo chown $(id -u):$(id -g) $XAUTHORITY
 fi
-sudo chown $(id -u):$(id -g) $XAUTHORITY
 
 if [ -f $HOME/.bashrc ]; then
     source $HOME/.bashrc
--- create-images.sh
+++ create-images.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -e
 
 cd `dirname $0`
-podman build ./100_base/ -t sabotagecla6/ubuntu
-podman build ./200_usr/  -t sabotagecla6/ubuntu-usr
-podman build ./300_gui/  -t sabotagecla6/ubuntu-gui
-podman build ./300_gui/  -t sabotagecla6/ubuntu-gui-jp -f ./300_gui/Containerfile_JP
+podman build --no-cache ./100_base/ -t sabotagecla6/ubuntu
+podman build --no-cache ./200_usr/  -t sabotagecla6/ubuntu-usr
+podman build --no-cache ./300_gui/  -t sabotagecla6/ubuntu-gui
+podman build --no-cache ./300_gui/  -t sabotagecla6/ubuntu-gui-jp -f ./300_gui/Containerfile_JP
 
 # application
-podman build ./500_application/chrome  -t sabotagecla6/chrome
-podman build ./500_application/vscode  -t sabotagecla6/vscode
+podman build --no-cache ./500_application/chrome  -t sabotagecla6/chrome
+podman build --no-cache ./500_application/vscode  -t sabotagecla6/vscode
+
+podman build --no-cache ./600_devenv/python -t sabotagecla6/python-dev
+
+podman build --no-cache ./700_runtime/python -t sabotagecla6/vscode
