a7a02f9481125041a7d7647444691520c72aef35
sabotage-cla6 <nine.gates111999@mail.com>
2026-05-31 17:35:03 +0900

    net=hostでなくても使用できるように修正

--- 300_gui/entrypoint.sh
+++ 300_gui/entrypoint.sh
@@ -3,6 +3,8 @@
 if [ -f /tmp/.Xauthority ];then
     sudo cp /tmp/.Xauthority $XAUTHORITY
     sudo chown $(id -u):$(id -g) $XAUTHORITY
+    COOKIE=$(xauth -f $XAUTHORITY list | head -n 1 | awk '{print $3}')
+    xauth add ${HOSTNAME}/unix:0 MIT-MAGIC-COOKIE-1 $COOKIE
 fi
 
 if [ -f $HOME/.bashrc ]; then
